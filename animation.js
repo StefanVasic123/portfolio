56b0a5fe3ac4f468b6953c6315e959a447c8b022
StefanVasic123 <52920937+StefanVasic123@users.noreply.github.com>
2019-11-16 01:30:16 +0100

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,29 @@
+const hero = document.querySelector('.hero');
+// const particlesJS = document.querySelector('#particles-js');
+const logo = document.querySelector('.logo');
+const hamburger = document.querySelector('.menu');
+const headline = document.querySelector('.headline');
+const herobox = document.querySelector('#hero-box');
+
+const tl = new TimelineMax();
+
+tl.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
+.fromTo(
+    hero, 
+    3, 
+    {width: "100%"}, 
+    {width: "0%", ease: Power2.easeInOut},)
+/*.fromTo(particlesJS, 
+    1.2, 
+    {x: "-100%"}, 
+    {x: "0%", ease: Power2.easeInOut}, "-=1.2")*/
+.fromTo(
+    logo, 1.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=1.5")
+.fromTo(
+    hamburger, 1.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=1.5");
+
+
+
+
+
+
